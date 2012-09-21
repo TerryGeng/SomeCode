e6690553447554ca683a753ed2d118f7a84255cd
TerryGeng <gengyanda@gmail.com>
2012-09-21 23:58:13 +0800

    Add note to the Bin2dec.cpp

--- Binary Switch/Bin2dec.cpp
+++ Binary Switch/Bin2dec.cpp
@@ -1,4 +1,6 @@
-#include<cstdio>
+// Binary to decimal
+// Switch binary number from the file "input.txt" and put the result into "output.txt".(max length of single binary number is 3000,"-1" means the end of the input.)
+ 
 #include<cmath>
 #include<fstream>
 #include<cstring>
@@ -32,7 +34,7 @@ double Bin_to_Dec(char* orginal,int size){
   double temp = 0;
 
   for(int i = 0;i<size;++i){
-    temp += (*(orginal + size - i - 1) - 48) * pow(2,i);
+    temp += (*(orginal + size - i - 1) - 48) * pow(2,i); //The ascii code of "0" is "48".
   }
 
   return temp;
