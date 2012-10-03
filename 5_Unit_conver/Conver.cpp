a69c37f8d65407556b78f6c7cb64051817ab51be
TerryGeng <gengyanda@gmail.com>
2012-10-03 13:07:25 +0800

    [5]Complete this programme.

--- 5_Unit_conver/Conver.cpp
+++ 5_Unit_conver/Conver.cpp
@@ -8,14 +8,16 @@ using namespace std;
 
 
 struct tagUnitConv {
-  string convF;
-  string convT;
-  int ex;
+  string ori;
+  string des;
+  double value;
 };
 
 typedef struct tagUnitConv UnitConv;
 
 int load_units(const char* filename);
+double units_conver(string ori,string des,double value);
+double get_conver_value(string ori,string des);
 
 vector<UnitConv> Units;
 
@@ -23,17 +25,26 @@ vector<UnitConv> Units;
 
 int main(){
 
-  string convF,convT;
+  string ori,des;
   double input = 0;
+  int loaded = load_units("Units.txt");
 
-  load_units("Units.txt");
-
-  cout<<"Please type-in the number. \n\n"
-    <<"x * 10 ^ y \n"
-    <<"x : \n";
-
+  cout<<"Units conver \n"
+    <<"-------------\n"
+    <<"Loaded: "<<loaded<<" Units.\n\n"
+    <<"Original unit: ";
+  cin>>ori;
+  cout<<"\nObjective unit: ";
+  cin>>des;
+  cout<<"\nValue: ";
   cin>>input;
 
+  if(!input) cout<<"You input a wrong number or zero.";
+  else{
+    double after = units_conver(ori,des,input);
+    if(!after) cout<<"\nFailed.Have you add corresponding unit-info into Units Table yet? \n";
+    else cout<<"\nResult: "<<after<<" "<<des<<"\n";
+  }
 
   cin.ignore();
 
@@ -46,25 +57,36 @@ int main(){
 // 
 // @return val:The number of units which loaded.
 int load_units(const char* filename){
-
   fstream input;
   istringstream line;
-
   input.open(filename,fstream::in);
 
   UnitConv temp;
-  for(string str;getline(input,str);){
 
+  Units.clear();
+  for(string str;getline(input,str);){
     line.str(str);
 
-    Units.clear();
-
-    line>>temp.convF>>temp.convT>>temp.ex;
-
+    line>>temp.ori>>temp.des>>temp.value;
     Units.push_back(temp);
 
   }
 
   return Units.size();
 
+}
+
+double units_conver(string ori,string des,double value){
+  double conver_tag = get_conver_value(ori,des);
+  if(conver_tag == -1) return 0;
+
+  return value * conver_tag;
+}
+
+double get_conver_value(string ori,string des){
+  for(int i=0;i<Units.size();i++){
+   if(Units[i].ori == ori && Units[i].des == des) return Units[i].value; 
+  }
+
+  return -1;
 }
