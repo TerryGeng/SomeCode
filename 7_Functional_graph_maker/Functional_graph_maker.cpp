4296534447586c1ad7100ede3f20c5e46cb2f80e
TerryGeng <gengyanda@gmail.com>
2012-10-20 16:22:30 +0800

    [7]Fixed the problem:Map can't work well with negative number

--- 7_Functional_graph_maker/Functional_graph_maker.cpp
+++ 7_Functional_graph_maker/Functional_graph_maker.cpp
@@ -1,10 +1,15 @@
 // A Functional graph drawing programme.
 // 
+// The graph is formed of chars;
+//
 // Can deal with many function(E.g. liner function...).
 //
-// I'm working on adding float support.So I create a new branch.
+// This programme used a lot of STL containers.Such as map,vector.But maybe it's not a good idea to use a double as the key of a map,especially use a negative double,it can cause some 
+// strange problem such as you have already added a key,but you can't use find() to find the key. At very first,I tried to use map to store the coordinates.Later,It proved that I was 
+// wrong.I have to add a number to each coordnate to keep them positive.Since map can't work well,I considered that I should use vector instead of map,but I'm not patient enough to keep
+// maintaining it.
 //
-// This programme hasn'n completed yet.I will complete this programme and merge this branch into the master in a few days.
+// If you want to change the function you want to draw,just change the function f.
 //
 //-----------
 
@@ -50,7 +55,7 @@ void getEmptyCoordMap(CoordMap& map,Range range,double unitlen);
 double unitePrecision(double value,double precision);
 
 double f(double x){
-  return x;
+  return 1/x+5;
 }
 
 //------------
@@ -60,11 +65,11 @@ int main(){
   CoordMap map;
   Range range;
 
-  range.xRangeStart=6;
-  range.xRangeEnd=-6;
-  range.yRangeStart=6;
-  range.yRangeEnd=-6;
-  range.steplen=2;
+  range.xRangeStart=10;
+  range.xRangeEnd=-10;
+  range.yRangeStart=10;
+  range.yRangeEnd=0;
+  range.steplen=0.1;
 
   coordSet = genCoordSet(f,range);
   CoordSetToCoordMap(coordSet,map,range);
@@ -75,7 +80,10 @@ int main(){
 
 //------------
 void printCoordMap(CoordMap& map){
+  cout<<"Graph:\n";
   for(CoordMap::iterator it=(--map.end());it!=(--map.begin());it--){
+    cout.precision(5);
+    //cout<<fixed<<(*it).first<<" ";
     Column& col = (*it).second;
     for(Column::iterator cIt=col.begin();cIt!=col.end();cIt++){
       cout<<((*cIt).second ? "*" : " ");
@@ -97,6 +105,9 @@ vector<Coord> genCoordSet(Func* f,Range range){
 
     if(temp.y>range.yRangeStart || temp.y<range.yRangeEnd) continue;
 
+    temp.x=fabs(temp.x-range.xRangeEnd);
+    temp.y=fabs(temp.y-range.yRangeEnd);
+
     CoordSet.push_back(temp);
   }
 
@@ -105,6 +116,7 @@ vector<Coord> genCoordSet(Func* f,Range range){
 
 void CoordSetToCoordMap(vector<Coord>& coordSet,CoordMap& map,Range range){
   getEmptyCoordMap(map,range,range.steplen);
+  //cout<<"CoordSetToCoordMap:"<<coordSet.size()<<"\n";
 
   double tempY = 0;
 
@@ -113,21 +125,21 @@ void CoordSetToCoordMap(vector<Coord>& coordSet,CoordMap& map,Range range){
 
     if(map.find(tempY) != map.end()){
       map[tempY][coordSet[i].x] = 1;
-      cout<<coordSet[i].x<<" "<<tempY<<"\n";
+      //cout<<"O Y "<<tempY<<" X "<<coordSet[i].x<<" "<<"\n";
     }else{
-      cout<<"N "<<coordSet[i].x<<" "<<tempY<<" "<<"\n";
+      //cout<<"N Y "<<tempY<<" X "<<coordSet[i].x<<" "<<"\n";
     }
   }
 }
 
 void getEmptyCoordMap(CoordMap& map,Range range,double unitlen){
+  //cout<<"getEmptyCoordMap:\n";
   Column temp;
-  for(double x=range.xRangeEnd;x<=range.xRangeStart;x+=unitlen){
-    cout<<"x:"<<x<<"\n";
+  for(double x=0;x<=fabs(range.xRangeStart-range.xRangeEnd);x+=unitlen){
     temp[x] = 0;
   }
-  for(double y=range.yRangeEnd;y<=range.yRangeStart;y+=unitlen){
-    cout<<"y:"<<y<<"\n";
+  for(double y=0;y<=fabs(range.yRangeStart-range.yRangeEnd);y+=unitlen){
+    //cout<<"Y "<<y<<"\n";
     map[y] = temp;
   }
 
@@ -145,8 +157,8 @@ double unitePrecision(double value,double precision){
   while((temp = sum + precision) < value){
     sum = temp;
   }
-
-  if((value - sum) > precision/2){
+  double half = precision/2;
+  if(((value - sum) - half) >= -0.0000001){
     sum += precision;
   }
 
