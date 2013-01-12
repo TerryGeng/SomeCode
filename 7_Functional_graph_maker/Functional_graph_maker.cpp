3a00db9f0b4a61466953b7a7325bdbe5822aef3e
TerryGeng <gengyanda@gmail.com>
2013-01-12 18:55:13 +0800

    [7]Change datastruct into vector and update related function.

--- 7_Functional_graph_maker/Functional_graph_maker.cpp
+++ 7_Functional_graph_maker/Functional_graph_maker.cpp
@@ -1,61 +1,28 @@
-// A Functional graph drawing programme.
-// 
-// The graph is formed of chars;
-//
-// Can deal with many function(E.g. liner function...).
-//
-// This programme used a lot of STL containers.Such as map,vector.But maybe it's not a good idea to use a double as the key of a map,especially use a negative double,it can cause some 
-// strange problem such as you have already added a key,but you can't use find() to find the key. At very first,I tried to use map to store the coordinates.Later,It proved that I was 
-// wrong.I have to add a number to each coordnate to keep them positive.Since map can't work well,I considered that I should use vector instead of map,but I'm not patient enough to keep
-// maintaining it.
-//
-// If you want to change the function you want to draw,just change the function f.
-//
-//-----------
-
 #include<iostream>
 #include<cmath>
 #include<vector>
-#include<map>
 
 using namespace std;
 
 //------------
 
-struct CoordTag {
+typedef struct {
   double x;
   double y;
-};
-
-struct RangeTag{
-  double xRangeStart;
-  double xRangeEnd;
-  double yRangeStart;
-  double yRangeEnd;
-  double steplen;
-};
-
-typedef struct CoordTag Coord;
-typedef struct RangeTag Range;
+} Coord;
 
 typedef double Func(double x);
 
-typedef map<double,bool> Column;
-typedef map<double,Column> CoordMap;
+typedef vector< vector<int> > CoordMap;
 
 //------------
 void printCoordMap(CoordMap&);
-
-vector<Coord> genCoordSet(Func*,Range range);
-
-void CoordSetToCoordMap(vector<Coord>& coordSet,CoordMap& map,Range range);
-
-void getEmptyCoordMap(CoordMap& map,Range range,double unitlen);
-
-double unitePrecision(double value,double precision);
+vector<Coord> genCoordSet(Func*,double,double,double);
+int CoordSetToMap(vector<Coord>&,CoordMap&,int,int,double);
+void genEmptyCoordMap(CoordMap&,int,int);
 
 double f(double x){
-  return 1/x+5;
+  return pow(x,2);
 }
 
 //------------
@@ -63,16 +30,9 @@ double f(double x){
 int main(){
   vector<Coord> coordSet;
   CoordMap map;
-  Range range;
 
-  range.xRangeStart=10;
-  range.xRangeEnd=-10;
-  range.yRangeStart=10;
-  range.yRangeEnd=0;
-  range.steplen=0.1;
-
-  coordSet = genCoordSet(f,range);
-  CoordSetToCoordMap(coordSet,map,range);
+  coordSet = genCoordSet(f,-10,10,0.5);
+  CoordSetToMap(coordSet,map,40,40,0.2);
   printCoordMap(map);
 
   return 0;
@@ -81,32 +41,24 @@ int main(){
 //------------
 void printCoordMap(CoordMap& map){
   cout<<"Graph:\n";
-  for(CoordMap::iterator it=(--map.end());it!=(--map.begin());it--){
-    cout.precision(5);
-    //cout<<fixed<<(*it).first<<" ";
-    Column& col = (*it).second;
-    for(Column::iterator cIt=col.begin();cIt!=col.end();cIt++){
-      cout<<((*cIt).second ? "*" : " ");
+  for(int i=0;i<map.size();i++){
+    for(int j=0;j<map[i].size();j++){
+      cout<<(map[i][j] ? "*" : " ");
     }
 
     cout<<"\n";
   }
 }
 
-vector<Coord> genCoordSet(Func* f,Range range){
+vector<Coord> genCoordSet(Func* f,double start,double end,double steplen){
   vector<Coord> CoordSet;
   Coord temp;
 
   CoordSet.reserve(20);
 
-  for(double x=range.xRangeEnd;x<=range.xRangeStart;x+=range.steplen){
+  for(double x=start;x<=end;x+=steplen){
     temp.x = x;
     temp.y = f(x);
-
-    if(temp.y>range.yRangeStart || temp.y<range.yRangeEnd) continue;
-
-    temp.x=fabs(temp.x-range.xRangeEnd);
-    temp.y=fabs(temp.y-range.yRangeEnd);
 
     CoordSet.push_back(temp);
   }
@@ -114,55 +66,26 @@ vector<Coord> genCoordSet(Func* f,Range range){
   return CoordSet;
 }
 
-void CoordSetToCoordMap(vector<Coord>& coordSet,CoordMap& map,Range range){
-  getEmptyCoordMap(map,range,range.steplen);
-  //cout<<"CoordSetToCoordMap:"<<coordSet.size()<<"\n";
-
-  double tempY = 0;
+int CoordSetToMap(vector<Coord>& coordSet,CoordMap& map,int width,int height,double unitlen){
+  genEmptyCoordMap(map,width,height);
 
   for(int i=0;i<coordSet.size();i++){
-    tempY = unitePrecision(coordSet[i].y,range.steplen);
+    int x,y;
+    x = coordSet[i].x/unitlen + width/2; 
+    y = coordSet[i].y/unitlen + height/2;
 
-    if(map.find(tempY) != map.end()){
-      map[tempY][coordSet[i].x] = 1;
-      //cout<<"O Y "<<tempY<<" X "<<coordSet[i].x<<" "<<"\n";
-    }else{
-      //cout<<"N Y "<<tempY<<" X "<<coordSet[i].x<<" "<<"\n";
-    }
+    if(x>map.size() || y>map[x].size()) continue;
+
+    map[x][y] = 1;
+  }
+
+  return 1;
+}
+
+void genEmptyCoordMap(CoordMap& map,int width,int height){
+  vector<int> column(height,0);
+  for(int i=0;i<width;i++){
+    map.push_back(column);
   }
 }
 
-void getEmptyCoordMap(CoordMap& map,Range range,double unitlen){
-  //cout<<"getEmptyCoordMap:\n";
-  Column temp;
-  for(double x=0;x<=fabs(range.xRangeStart-range.xRangeEnd);x+=unitlen){
-    temp[x] = 0;
-  }
-  for(double y=0;y<=fabs(range.yRangeStart-range.yRangeEnd);y+=unitlen){
-    //cout<<"Y "<<y<<"\n";
-    map[y] = temp;
-  }
-
-}
-
-double unitePrecision(double value,double precision){
-  double sign=1;
-
-  if(value < 0) sign = -1;
-
-  double sum=0,temp=0;
-
-  value = fabs(value);
-
-  while((temp = sum + precision) < value){
-    sum = temp;
-  }
-  double half = precision/2;
-  if(((value - sum) - half) >= -0.0000001){
-    sum += precision;
-  }
-
-  sum *= sign;
-
-  return sum;
-}
